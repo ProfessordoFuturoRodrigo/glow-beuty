b543e970d805fc30f3da565886b6e640a17b9240
Rodrigo Souto <professordofuturorodrigo@gmail.com>
2025-11-28 08:24:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const produtos = [
   {
     nome: "Batom Matte Rosê",
     preco: 39.90,
-    imagem: "https://cdn.pixabay.com/photo/2016/11/19/14/00/lipstick-1839414_1280.jpg"
+    imagem: "batom-safe-red.webp"
   },
   {
     nome: "Base Líquida Natural",
     preco: 59.90,
-    imagem: "https://cdn.pixabay.com/photo/2016/03/27/20/51/cosmetics-1281661_1280.jpg"
+    imagem: "batom-safe-red.webp"
   },
   {
     nome: "Máscara de Cílios Volume Max",
     preco: 49.90,
-    imagem: "https://cdn.pixabay.com/photo/2016/03/27/19/50/cosmetics-1281508_1280.jpg"
+    imagem: "batom-safe-red.webp"
   },
   {
     nome: "Paleta de Sombras Glam",
     preco: 89.90,
-    imagem: "https://cdn.pixabay.com/photo/2017/01/09/14/48/makeup-1967762_1280.jpg"
+    imagem: "batom-safe-red.webp"
   }
 ];
 
